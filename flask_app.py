3fdc6efdf8b70ebd91f340f5ce8e8b5ec8393e21
bdoran <bobbydoran13@gmail.com>
2019-04-27 17:39:29 +0000

    added readme and path to github

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,3 +9,14 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, this is a basic message.'
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('./myproject')
+        origin = repo.remotes.origin
+        repo.create_head('master',
+        origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+        origin.pull()
+        return '', 200
+    else:
+        return '', 400
