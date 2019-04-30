e2a1ebd35bd5246f00871f04f76d1718cb5b0228
bdoran <bobbydoran13@gmail.com>
2019-04-30 00:28:28 +0000

    added prebuilt bootstrap css to style

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     return render_template("main_page.html")
 
