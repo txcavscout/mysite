6ce7e5be94e4759d11777e510648c5f5965fad2b
bdoran <bobbydoran13@gmail.com>
2019-04-30 00:39:08 +0000

    Added functionality to post method

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,18 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+comments = []
 
-@app.route('/', methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
+
 
